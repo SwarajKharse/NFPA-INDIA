20e697ad9d518871b4ad162bea47959ae3af487d
TechTrailblazerSK <swarajkharse2808@gmail.com>
2024-07-08 22:24:41 +0530

    Footer bug Solved

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,9 @@
 import { Component, ElementRef, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
-
-import { Storage, ref, getDownloadURL } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-footer',
@@ -14,6 +12,7 @@ import { Storage, ref, getDownloadURL } from '@angular/fire/storage';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
+
 export class FooterComponent implements AfterViewInit{
 
   @ViewChildren('footer') footer!: QueryList<ElementRef>;
@@ -26,38 +25,22 @@ export class FooterComponent implements AfterViewInit{
   @ViewChildren('mail') mail!: QueryList<ElementRef>;
   @ViewChildren('developer') developer!: QueryList<ElementRef>;
 
-  logoImage: string | null = null;
-  instagram: string | null = null;
-  facebook: string | null = null;
-  youtube: string | null = null;
-  whatsapp: string | null = null;
-  twitter: string | null = null;
+  constructor(private router: Router) {}
 
-  constructor(private storage: Storage) {}
-
-  async ngOnInit() {
-
-    const imageReference = ref(this.storage, 'gs://nfpa-website.appspot.com/icons/NavbarFooter/logo.png');
-    this.logoImage = await getDownloadURL(imageReference);
-
-    const imageRef1 = ref(this.storage, 'gs://nfpa-website.appspot.com/icons/NavbarFooter/instagram.png');
-    this.instagram = await getDownloadURL(imageRef1);
-
-    const imageRef2 = ref(this.storage, 'gs://nfpa-website.appspot.com/icons/NavbarFooter/facebook.png');
-    this.facebook = await getDownloadURL(imageRef2);
-
-    const imageRef3 = ref(this.storage, 'gs://nfpa-website.appspot.com/icons/NavbarFooter/youtube.png');
-    this.youtube = await getDownloadURL(imageRef3);
-
-    const imageRef4 = ref(this.storage, 'gs://nfpa-website.appspot.com/icons/NavbarFooter/whatsapp.png');
-    this.whatsapp = await getDownloadURL(imageRef4);
-
-    const imageRef5 = ref(this.storage, 'gs://nfpa-website.appspot.com/icons/NavbarFooter/twitter.png');
-    this.twitter = await getDownloadURL(imageRef5);
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ScrollTrigger.refresh();
+        this.setupFooterAnimation();
+      }
+    });
   }
 
   ngAfterViewInit() {
+    this.setupFooterAnimation();
+  }
 
+  setupFooterAnimation() {
     const footer = this.footer.find((el, index) => index === 0)?.nativeElement;
     const logo = this.logo.map((el) => el.nativeElement);
     const aboutText = this.aboutText.map((el) => el.nativeElement);
@@ -125,6 +108,5 @@ export class FooterComponent implements AfterViewInit{
       { opacity: 1, duration: 0.7, ease: 'power2.inout' },
       '-=1.2'
     );
-
   }
 }
