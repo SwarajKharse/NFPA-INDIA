4cfe9f15e11cd4a8157b0dd837ce33132a93e49a
Rudraksha M J <143385610+rmj2626@users.noreply.github.com>
2024-07-18 15:41:20 +0530

    Update fire-extinguisher-refilling.component.ts

--- src/app/total-systems/fire-extinguisher-refilling/fire-extinguisher-refilling.component.ts
+++ src/app/total-systems/fire-extinguisher-refilling/fire-extinguisher-refilling.component.ts
@@ -11,9 +11,15 @@ import { CartService } from '../cartService/cart.services';
 export class FireExtinguisherRefillingComponent {
   constructor(private cartService: CartService) {}
 
-  addProductToCart(productName: string) {
-    this.cartService.addProduct(productName);
-    console.log('Products in cart:', this.cartService.getProducts());
+  addProductToCart(event: Event, productName: string) {
+    const buttonElement = (event.target as HTMLElement).closest('button');
+
+    // Check if the product is already in the cart
+    if (buttonElement && buttonElement.innerText.trim() === 'Add To Cart For Enquiry') {
+      this.cartService.addProduct(productName);
+      buttonElement.innerText = 'Added';
+      console.log('Products in cart:', this.cartService.getProducts());
+    }
   }
 
 }
