1f22c7bcb9ff43c69daddd4b9d567dbf4f448a25
Rudraksha M J <143385610+rmj2626@users.noreply.github.com>
2024-07-18 12:22:06 +0530

    Update schools.component.ts

--- src/app/total-solutions/schools/schools.component.ts
+++ src/app/total-solutions/schools/schools.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-schools',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './schools.component.html',
   styleUrl: '../sol-style.css'
 })
@@ -15,30 +16,35 @@ export class SchoolsComponent {
       name: 'Fire Extinguishers ',
       description: 'Fire extinguishers and cabinets are essential fire safety equipment, ready to combat small fires before they escalate. They provide a quick and accessible way to extinguish flames, especially crucial in the initial moments of a fire.',
       image: 'FE1.webp',
+      link: '/total-systems/Extinguishers',
     },
     {
       id: 2,
       name: 'Fire Detection Systems',
       description: 'Fire detection systems are the backbone of any fire safety plan, serving as the first line of defense in alerting occupants of potential danger. They play a critical role in ensuring timely evacuation and minimizing risks during a fire emergency.',
       image: 'Alarm-system.webp',
+      link: '/total-systems/Detection-System'
     },
     {
       id: 3,
       name: 'Fire Sprinkler Systems',
       description: 'Fire sprinkler systems act as an automatic fire suppression system, providing a critical layer of protection in buildings. A network of pipes filled with water reaches throughout the structure, with sprinkler heads strategically placed at intervals.',
       image: 'Sprinkler.webp',
+      link: '/total-systems/Sprinkler-System'      
     },
     {
       id: 4,
       name: 'Fire Hydrant System',
       description: 'Fire Hydrant System are the muscle behind water-based fire protection systems in buildings, especially high-rises and facilities with complex fire suppression needs. They act as pressure boosters, taking water from a municipal supply, reservoir, or tank and significantly increasing its pressure.',
       image: 'Fire Pump.webp',
+      link: '/total-systems/Hydrant-System'
     },
     {
       id: 5,
       name: 'Electrical Panel Suppression System',
       description: 'Fire suppression systems form a crucial line of defense in buildings, employing various technologies to extinguish flames and protect lives. Fire alarms with smoke and heat detectors trigger early warnings, while automatic sprinkler systems douse flames with water.',
-      image: 'Fire Suppression.webp',
+      image: 'electric-carousel-1.webp',
+      link: '/total-systems/Suppression-System'
     }
   ];
 
@@ -48,36 +54,42 @@ export class SchoolsComponent {
       name: 'Annual Maintenance Contracts(AMCs)',
       description: 'An Annual Maintenance Contract (AMC) for fire safety systems is a comprehensive service agreement designed to ensure the continued reliability and effectiveness of a building\'s fire protection systems.',
       image: 'AMC.webp',
+      link: '/total-systems/AMC'
     },
     {
       id: 2,
-      name: 'One-Time Repairs',
-      description: 'One-Time Repairs is a flexible service designed to address specific, non-recurring issues in fire safety systems or equipment. This service caters to clients who need immediate fixes for malfunctioning fire safety components without committing to long-term maintenance contracts.',
-      image: 'One-TimeRepair.webp',
+      name: 'Fire Fighting Audits',
+      description: 'Fire fighting audits are thorough evaluations of a building\'s fire safety measures, conducted to ensure compliance with fire safety regulations and to identify potential risks. These audits involve a detailed inspection of all fire protection systems, such as fire alarms, sprinkler systems, fire extinguishers, and emergency exits.',
+      image: 'Fire-Audit.webp',
+      link: '/total-systems/Audit'
     },
     {
       id: 3,
-      name: 'Fire Fighting Audits',
-      description: 'Fire audits are thorough evaluations of a building\'s fire safety measures, conducted to ensure compliance with fire safety regulations and to identify potential risks. These audits involve a detailed inspection of all fire protection systems, such as fire alarms, sprinkler systems, fire extinguishers, and emergency exits.',
-      image: 'Fire-Audit.webp',
+      name: 'Basic and Advanced Fire Fighting & First Aid Training',
+      description: 'Basic and Advanced Fire Fighting & First Aid Training is an essential program designed to equip individuals with fundamental skills and knowledge to respond effectively to fire emergencies. This training covers critical aspects of fire safety, including fire prevention, proper use of fire extinguishers, and evacuation procedures.',
+      image: 'FTraining.webp',
+      link: '/total-systems/Training'
     },
     {
       id: 4,
-      name: 'Fire Fighting Licensing (NOC)',
-      description: 'Fire Fighting Licensing (NOC) is a critical regulatory process that involves obtaining a No Objection Certificate (NOC) from the local fire department or relevant authority for fire safety compliance. This licensing ensures that buildings, businesses, or events meet the required fire safety standards and regulations.',
-      image: 'NOC.webp',
-    },
-    {
-      id: 5,
-      name: 'Basic Fire Fighting Training',
-      description: 'Basic Fire Fighting Training is an essential program designed to equip individuals with fundamental skills and knowledge to respond effectively to fire emergencies. This training covers critical aspects of fire safety, including fire prevention, proper use of fire extinguishers, and evacuation procedures.',
-      image: 'FTraining.webp',
-    },
-    {
-      id: 6,
       name: 'Fire Extinguisher Refilling',
       description: 'Fire Extinguisher Refilling is a crucial maintenance service that ensures fire extinguishers remain fully operational and ready for use in case of emergencies. This service involves recharging depleted extinguishers with the appropriate extinguishing agent to restore their firefighting capabilities.',
       image: 'FERefill.webp',
+      link: '/total-systems/Refill'
     },
+    {
+      id: 5,
+      name: 'Fire Fighting Licensing (NOC)',
+      description: 'Fire Fighting Licensing (NOC) is a critical regulatory process that involves obtaining a No Objection Certificate (NOC) from the local fire department or relevant authority for fire safety compliance. This licensing ensures that buildings, businesses, or events meet the required fire safety standards and regulations.',
+      image: 'NOC.webp',
+      link: '/total-systems/Licensing'
+    },
+    {
+      id: 6,
+      name: 'One-Time Repairs',
+      description: 'One-Time Repairs is a flexible service designed to address specific, non-recurring issues in fire safety systems or equipment. This service caters to clients who need immediate fixes for malfunctioning fire safety components without committing to long-term maintenance contracts.',
+      image: 'One-TimeRepair.webp',
+      link: '/total-systems/Repair'
+    }
   ];
 }
